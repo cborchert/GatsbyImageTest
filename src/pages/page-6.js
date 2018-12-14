bbf7a9091ebd7e679f3dba48ca3bead0b8030a66
Chris Borchert <cborchertrun@gmail.com>
2018-12-14 18:41:17 +0100

    Update header

--- src/pages/page-6.js
+++ src/pages/page-6.js
@@ -8,9 +8,9 @@ const arr = new Array(110).fill(0).map((a, i) => i + 1)
 
 const IndexPage = () => (
   <Layout>
-    <h1>Loading 200 individual images, the old fashioned way</h1>
+    <h1>Loading 1 Image, the old fashioned way</h1>
     <div className="image-grid">
-      <img src={Image1} alt="}" />
+      <img src={Image1} alt="Image1" />
     </div>
   </Layout>
 )
